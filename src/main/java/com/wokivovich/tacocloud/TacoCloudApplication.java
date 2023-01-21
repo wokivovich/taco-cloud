983fc2685205918165569b4ae0f7d0504f2bf228
Alexander Nikulin <wokiwokivovich@gmail.com>
2023-01-21 17:19:32 +0300

    initial commit

---
+++ src/main/java/com/wokivovich/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.wokivovich.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/wokivovich/tacocloud/entity/Taco.java
@@ -0,0 +1,20 @@
+package com.wokivovich.tacocloud.entity;
+
+import com.wokivovich.tacocloud.entity.Ingredient;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+public class Taco {
+
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
+    private String name;
+
+    @NotNull
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    private List<Ingredient> ingredients;
+}
